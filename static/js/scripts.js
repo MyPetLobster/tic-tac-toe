92397f215b02755888730d4997b54225058b3aa9
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-07-21 11:26:42 -0400

    Create CLI version of game, update gitignore

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,88 +1,114 @@
-const boardSize = 3
-
-// for (let i = 0; i < boardSize; i++) {
-//     if (i === 2) {
-//         console.log(" | | ");
-//     } else {
-//         console.log("_|_|_");
-//     }
-// }
-
-// Create 2d array 
-
-// Visualization of board
-
-// '0, 0' | '0, 1' | '0, 2'
-// -------|--------|-------
-// '1, 0' | '1, 1' | '1, 2'
-// -------|--------|-------
-// '2, 0' | '2, 1' | '2, 2' 
+// Gameboard module
+const gameBoard = (() => {
+    let board = ["", "", "", "", "", "", "", "", ""];
+    const getBoard = () => board;
+    const setBoard = (index, value) => board[index] = value;
+    const resetBoard = () => board = ["", "", "", "", "", "", "", "", ""];
+    return { getBoard, setBoard, resetBoard };
+})
 
 
+// Player factory function
+const Player = (name, marker) => {
+    return { name, marker };
+}
 
+// Game module
+const game = (() => {
+    let player1 = Player("Player 1", "X");
+    let player2 = Player("Player 2", "O");
+    let currentPlayer = player1;
+    let winner = null;
+    let gameOver = false;
+    let moves = 0;
 
-
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-function createBoard (rows=3, cols=3) {
-    const board = [];
-    for (let i = 0; i < rows; i++) {
-        board[i] = [];
-        for (let j = 0; j < cols; j++) {
-            board[i][j] = `${i}, ${j}`;
+    const checkWinner = () => {
+        const board = gameBoard().getBoard();
+        const winningCombos = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+            [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+            [0, 4, 8], [2, 4, 6] // Diagonals
+        ];
+        winningCombos.forEach(combo => {
+            if (board[combo[0]] == currentPlayer.marker && board[combo[1]] == currentPlayer.marker && board[combo[2]] == currentPlayer.marker) {
+                winner = currentPlayer;
+                gameOver = true;
+            }
+        });
+        if (moves === 9 && !winner) {
+            gameOver = true;
         }
     }
-    return board
-}
 
-function displayBoard (board) {
-    for (let i = 0; i < board[0]; i++) {
-        console.log('------');
-        for (let j = 0; j < board[0]; j++) {
-            console.log(`| ${board[i][j]} |`)
+    const switchPlayer = () => {
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
+
+    const playMove = (index) => { 
+        if (gameBoard().getBoard()[index] === "" && ! gameOver) {
+            gameBoard().setBoard(index, currentPlayer.marker);
+            moves++;
+            checkWinner();
+            switchPlayer();
         }
     }
-}
 
-
-function makeMove (player, board) {
-
-    readline.question(`player-${player}, make your move! (e.g. '0,0' for top left)`, (move) => {
-        console.log(move);
-        readline.close();
-    });
-    // const x = move.split(',')[0];
-    // const y = move.split(',')[1];
-    // board[x][y] = player === 1 ? "X" : "O";
-    // displayBoard(board);
-    // return board;
-}
-
-
-
-function playGame() {
-    board = createBoard(3,3);
-    let gameActive = true;
-    displayBoard(board);
-
-    let player1 = true;
-    while (gameActive) {
-        if (player1) {
-            board = makeMove(1, board);
-        } else {
-            board = makeMove(2, board);
-        }
-        player1 != player1;
+    const resetGame = () => {
+        gameBoard().resetBoard();
+        currentPlayer = player1;
+        winner = null;
+        gameOver = false;
+        moves = 0;
     }
-}
+
+    return { playMove, resetGame, winner, gameOver, currentPlayer };
+
+})
 
 
-function checkForWinner(board) {
+// Display module (CLI)
+const displayControllerCLI = (() => {
+    const displayBoard = () => {
+        const board = gameBoard().getBoard();
+        console.log(` ${board[0]} | ${board[1]} | ${board[2]} `);
+        console.log(`---|---|---`);
+        console.log(` ${board[3]} | ${board[4]} | ${board[5]} `);
+        console.log(`---|---|---`);
+        console.log(` ${board[6]} | ${board[7]} | ${board[8]} `);
+    }
+
+    const displayWinner = () => {
+        if (game().winner) {
+            console.log(`${game().winner.name} wins!`);
+        }
+    }
     
+    const displayCurrentPlayer = () => {
+        console.log(`${game().currentPlayer.name}'s turn`);
+    }
+
+    const displayGameOver = () => {
+        if (game().gameOver) {
+            console.log("Game over!");
+        }
+    }
+
+    return { displayBoard, displayWinner, displayCurrentPlayer, displayGameOver };
+})
+
+// Display module (DOM)
+// TODO: Implement displayControllerDOM
+
+
+
+function playGameCLI () {
+    displayControllerCLI().displayBoard();
+    while (!game().gameOver) {
+        displayControllerCLI().displayCurrentPlayer();
+        let move = prompt("Enter a number between 0 and 8: ");
+        game().playMove(move);
+        displayControllerCLI().displayBoard();
+    }
+    displayControllerCLI().displayWinner();
+    displayControllerCLI().displayGameOver();
 }
-
-
-playGame();
