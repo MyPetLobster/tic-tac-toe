1154d98c9d5268683d14d691ca173ecd9aee84e2
Cory Suzuki <6979547+MyPetLobster@users.noreply.github.com>
2024-07-25 11:23:37 -0400

    Misc JS adjustments
    - move winningConditions inside checkWinner func
    
    - add comments to minimax function

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,4 +1,4 @@
-// GAME BOARD MODULE 
+// GAME BOARD LOGIC MODULE
 const gameBoard = (() => {
     const EMPTY_BOARD = Array(9).fill("");
     let gameBoard = [...EMPTY_BOARD];
@@ -15,6 +15,8 @@ const gameBoard = (() => {
 })();
 
 
+
+
 // GAME CONTROLLER MODULE
 const gameController = (() => {
     let aiMode = false;
@@ -24,15 +26,16 @@ const gameController = (() => {
 
     let currentPlayerIsX = true;
     let gameActive = true;
-    const winningConditions = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],  // horizontal
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],  // vertical
-        [0, 4, 8], [2, 4, 6]              // diagonal
-    ];
-    
+
     const areWeBeingLazy = () => Math.random() < 0.05; // 5% chance of being lazy
 
     const checkWinner = () => {
+        const winningConditions = [
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],  // horizontal
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],  // vertical
+            [0, 4, 8], [2, 4, 6]              // diagonal
+        ];
+
         let winner = null;
         winningConditions.forEach(([a, b, c]) => {
             if (
@@ -69,6 +72,7 @@ const gameController = (() => {
             endScreen.style.backgroundColor = `var(--primary-${colorClass}-opc)`;
             endScreen.style.color = `var(--primary-${colorClass})`;
             if (!bestOf3Mode) {
+                // 'winner-btn' classes make the button move from btn bar to the endgame screen
                 restartButton.classList.add(`winner-btn-${colorClass}`);
             }
         } else {
@@ -80,11 +84,13 @@ const gameController = (() => {
         }
 
         if (!bestOf3Mode) {
+            // If bestOf3Mode is activated, the endgame screen will be hidden automatically
             restartButton.addEventListener("click", restartGame);
         }
 
         endScreen.classList.add("show");
 
+        // Best of 3 Mode Endgame Logic
         if (bestOf3Mode) {
             if (winner === "X") {
                 xScore++;
@@ -92,6 +98,8 @@ const gameController = (() => {
                 oScore++;
             }
             displayController.updateScoreboard();
+
+            // Display winner of best of 3 after 2 wins
             if (xScore === 2 || oScore === 2) {
                 winnerText.classList.add("winner-text-best-of-3");
                 winnerText.textContent = xScore === 2 ? "X wins Best of 3!" : "O wins Best of 3!";
@@ -103,6 +111,7 @@ const gameController = (() => {
                     oScore = 0;
                     restartGame();
                 });
+            // Automatically start new game after 2 seconds until someone wins best of 3
             } else {
                 setTimeout(() => {
                     endScreen.removeChild(winnerText);
@@ -126,11 +135,12 @@ const gameController = (() => {
         const currentPlayer = currentPlayerIsX ? "X" : "O";
         gameBoard.setGameBoard(index, currentPlayer);
 
+        // Lazy mode just shows a stick figure being caught off guard on duty for a split second
         if (areWeBeingLazy()) {
             displayController.setLazyIcon(cell, currentPlayer, () => {
                 displayController.updateCellIcon(cell, currentPlayer);
                 if (aiMode && currentPlayer === "X") {
-                    gameActive = false;
+                    gameActive = false;     // Prevent player from clicking while AI is 'thinking'
                     setTimeout(aiTurn, 500);
                 }
             });
@@ -147,7 +157,6 @@ const gameController = (() => {
             handleEndGame(winner);
             return;
         }
-
         if (checkTie()) {
             handleEndGame(null);
             return;
@@ -158,34 +167,33 @@ const gameController = (() => {
         displayController.setStatusColor();
     };
 
-
     // Minimax Algorithm
     const minimax = (board, isMaximizing) => {
-        const scores = { X: -10, O: 10, tie: 0 };
-
+        const scores = { X: -10, O: 10, tie: 0 };               // -10, 10 and 0 are arbitrary values. Could be -1, 1, 0, etc.
         const winner = checkWinner();
-        if (winner) {
+
+
+        if (winner) {                                           // Check for base cases (win, lose, tie)
             return scores[winner];
         }
-
         if (checkTie()) {
             return scores.tie;
         }
 
-        if (isMaximizing) {
-            let bestValue = -Infinity;
+        if (isMaximizing) {                                     // Maximizing player (aka always AI in my implementation)
+            let bestValue = -Infinity;                          // Negative infinity is the worst possible value for maximizing player
 
-            for (let i = 0; i < board.length; i++) {
-                if (board[i] === "") {
-                    board[i] = "O";
-                    let value = minimax(board, false);
-                    board[i] = "";
-                    bestValue = Math.max(value, bestValue);
+            for (let i = 0; i < board.length; i++) {            // Loop through each cell
+                if (board[i] === "") {                          // If cell is empty
+                    board[i] = "O";                             // Make the move
+                    let value = minimax(board, false);          // Recursive call with the new board state to simulate the minimizing player (value becomes the returned 'bestValue' from the recursive call)
+                    board[i] = "";                              // Undo the move
+                    bestValue = Math.max(value, bestValue);     // Compare each turn to the best value so far
                 }
             }
-            return bestValue;
+            return bestValue;                                   // Return best value 
         } else {
-            let bestValue = Infinity;
+            let bestValue = Infinity;                           // Same for minimizing player (in this case it's the AI simulating the human player's best move)
 
             for (let i = 0; i < board.length; i++) {
                 if (board[i] === "") {
@@ -200,21 +208,21 @@ const gameController = (() => {
     };
 
     const getBestMove = (board) => {
-        let bestMove = null;
-        let bestValue = -Infinity;
+        let bestMove = null;                                    // Initialize best move to null
+        let bestValue = -Infinity;                              // Initialize best value to negative infinity
 
-        for (let i = 0; i < board.length; i++) {
-            if (board[i] === "") {
-                board[i] = "O";
-                let moveValue = minimax(board, false);
-                board[i] = "";
-                if (moveValue > bestValue) {
-                    bestMove = i;
-                    bestValue = moveValue;
+        for (let i = 0; i < board.length; i++) {                // Loop through each cell
+            if (board[i] === "") {                              // If cell is empty
+                board[i] = "O";                                 // Make the move
+                let moveValue = minimax(board, false);          // Call minimax with the new board state
+                board[i] = "";                                  // Undo the move
+                if (moveValue > bestValue) {                    // If the move value is greater than the best value so far
+                    bestMove = i;                               // Update the best move
+                    bestValue = moveValue;                      // Update the best value
                 }
             }
         }
-        return bestMove;
+        return bestMove;                                        // Return the best move for AI to make
     };
 
     const aiTurn = () => {
@@ -237,7 +245,7 @@ const gameController = (() => {
         currentPlayerIsX = !currentPlayerIsX;
         displayController.setStatus(currentPlayerIsX ? "X's Turn" : "O's Turn");
         displayController.setStatusColor();
-        gameActive = true;
+        gameActive = true;      // Re-enable player clicks once AI has made its move
     };
 
     const restartGame = () => {
@@ -264,7 +272,7 @@ const gameController = (() => {
         displayController.setRestartButton("Restart");
     };
 
-    // Expose methods to get and set encapsulated variables
+    // Expose methods to get and set encapsulated variables (Fixes issue with encapsulating global variables)
     return { 
         handleCellClick, 
         restartGame, 
@@ -283,6 +291,8 @@ const gameController = (() => {
 })();
 
 
+
+
 // DISPLAY CONTROLLER MODULE
 const displayController = (() => {
     const cells = document.querySelectorAll(".cell");
@@ -290,17 +300,21 @@ const displayController = (() => {
     const restartButton = document.querySelector(".restart-button");
 
     const selectRandomIcon = (isX) => {
+        // There are six different icons for each player
         let num = Math.floor(Math.random() * 6) + 1;
         num = num < 10 ? "0" + num : num;  // format string for image path
         return isX ? `static/images/game_icons/x_icons/xman-${num}.png` : `static/images/game_icons/o_icons/oman-${num}.png`;
     };
 
-    const render = () => {
-        gameBoard.getGameBoard().forEach((value, index) => {
-            if (value === "") {
-                cells[index].innerHTML = "";
-            } 
-        });
+    const updateCellIcon = (cell, player) => {
+        const randomIcon = selectRandomIcon(player === "X");
+        cell.innerHTML = `<img src="${randomIcon}" alt="${player}">`;
+    };
+
+    const setLazyIcon = (cell, player, callback) => {
+        const lazyIcon = player === "X" ? "static/images/game_icons/lazy_icons/xman-lazy.png" : `static/images/game_icons/lazy_icons/oman-lazy.png`;
+        cell.innerHTML = `<img src="${lazyIcon}" alt="${player}">`;
+        setTimeout(callback, 500);
     };
 
     const setStatus = (message) => {
@@ -319,17 +333,6 @@ const displayController = (() => {
         restartButton.textContent = message;
     };
 
-    const updateCellIcon = (cell, player) => {
-        const randomIcon = selectRandomIcon(player === "X");
-        cell.innerHTML = `<img src="${randomIcon}" alt="${player}">`;
-    };
-
-    const setLazyIcon = (cell, player, callback) => {
-        const lazyIcon = player === "X" ? "static/images/game_icons/lazy_icons/xman-lazy.png" : `static/images/game_icons/lazy_icons/oman-lazy.png`;
-        cell.innerHTML = `<img src="${lazyIcon}" alt="${player}">`;
-        setTimeout(callback, 500);
-    };
-
     const updateScoreboard = () => {
         const mainH1 = document.getElementById("main-h1");
         const bestOf3Mode = gameController.getBestOf3Mode();
@@ -345,10 +348,21 @@ const displayController = (() => {
         }
     };
 
+    const render = () => {
+        gameBoard.getGameBoard().forEach((value, index) => {
+            if (value === "") {
+                cells[index].innerHTML = "";
+            } 
+        });
+    };
+
     return { render, setStatus, setStatusColor, setRestartButton, updateCellIcon, setLazyIcon, updateScoreboard };
 })();
 
 
+
+
+// INITIALIZATION and NAVIGATION MODULE
 const init = (() => {
     const checkCurrentPage = () => {
         const pageIdentifier = document.querySelector(".page-identifier");
